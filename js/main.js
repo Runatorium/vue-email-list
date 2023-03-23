4bd0184dc3d93c33eb389caab462cf7b698ecfe1
Runatorium <stefano.alesci1@gmail.com>
2023-03-23 15:07:00 +0100

    prove

--- js/main.js
+++ js/main.js
@@ -4,12 +4,36 @@ createApp(
     {
         data() {
             return {
-               
-            }   
+               randommail: " ",
+               maillist: [],
+            }  
         },
+
         methods: {
+            mailprint(){
+                console.log(this.randommail);
+            },
+            arraymailprint(){
+                for(i=0;i<this.mailprint.length; i++){
+                    console.log(this.maillist[i]);
+                }
+            },
+        },
+
+        mounted() {
             
-        } 
-          
+            for(i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+                
+                    this.randommail = response.data.response;
+                    this.maillist.push(this.randommail);  
+                    if(this.maillist.length == 10){
+                        console.log(this.maillist);
+                    }                  
+                }
+            )
+         }
     }
+}
 ).mount('#app')
